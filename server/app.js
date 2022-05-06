9753bb650381b0e2a05ce0abda5d8077fe176fa0
osadezu <osadezu@gmail.com>
2022-05-06 16:17:01 -0400

    Complete basic account creation and basic error handling

--- server/app.js
+++ server/app.js
@@ -7,17 +7,20 @@ const port = process.env.PORT || 4000; // Use default 4000 if not set in .env
 const express = require('express');
 const app = express();
 
+// Setup middleware
+app.use(express.json());
+
 // Controllers
 // Other resourece routes for the API would be added here
-app.use('api/accounts', require('./controllers/connectedAccounts'));
+app.use('/api/accounts', require('./controllers/connectedAccounts'));
 // app.use('api/balances', require('./controllers/balances'));
 // app.use('api/payments', require('./controllers/payments'));
 
-// Error Handling
+// Catch-all error handler
 app.use((err, req, res, next) => {
-  const statusCode = res.statusCode || 500;
-  const message = err.message;
-  res.status(statusCode).send(message);
+  console.error('Unhandled: ' + err.stack);
+  res.status(500);
+  res.json({ error: err });
 });
 
 // Listen for requests
--- server/controllers/connectedAccounts.js
+++ server/controllers/connectedAccounts.js
@@ -2,44 +2,52 @@ const router = require('express').Router();
 
 // Get Stripe instance
 // This was initialized with secret key in providers script
-const stripe = require('./providers/stripe');
+const stripe = require('../providers/stripe');
 
 // Routes
 
 // Index - list all connected accounts
 // This partially developed route is shown here as an example
 // illustrating further controller organization
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const { someOptions } = req.params;
-//     const accounts = await stripe.accounts.list({ someOptions });
-//     return res.json(accounts);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// Create a connected accountgu
-router.post('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const options = req.params;
-    console.log(options);
-
-    const parameters = {
-      type: 'custom',
-      country: 'US',
-      email: 'jenny.rosen@example.com',
-      capabilities: {
-        card_payments: { requested: true },
-        transfers: { requested: true },
-      },
-    };
-
-    const account = await stripe.accounts.create(parameters);
-
-    return res.json(account);
+    // const { someOptions } = req.params;
+    const accounts = await stripe.accounts.list();
+    return res.json(accounts.data);
   } catch (error) {
     next(error);
+  }
+});
+
+// Create a connected account
+router.post('/', async (req, res) => {
+  try {
+    const parameters = req.body;
+    // const parameters = {
+    //   type: 'custom',
+    //   country: 'US',
+    //   email: 'jenny.rosen@example.com',
+    //   capabilities: {
+    //     card_payments: { requested: true },
+    //     transfers: { requested: true },
+    //   },
+    // };
+
+    const account = await stripe.accounts.create(parameters);
+    console.log('Account created: ' + account.id);
+    res.json(account);
+  } catch (err) {
+    // Handle inavlid request error
+    if (err.statusCode === 400) {
+      res.status(err.statusCode);
+      res.json({
+        message: err.message,
+        error: err,
+      });
+    } else {
+      // Forward to catch-all error handler
+      next(err);
+    }
   }
 });
 
