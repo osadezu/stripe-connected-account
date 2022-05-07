186c9ef7ae924de067812638795f2e153178d2a1
osadezu <osadezu@gmail.com>
2022-05-06 20:06:24 -0400

    Create basic app scaffolding and router paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import CreateAccount from './components/CreateAccount';
+import ListAccounts from './components/ListAccounts';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<About />} />
+          <Route path='create' element={<CreateAccount />} />
+          <Route path='list' element={<ListAccounts />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 // Importing the Bootstrap CSS
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
